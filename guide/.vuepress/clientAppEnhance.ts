b2d090c9c825e1b2919ee9ab7917face8351fdb4
Joel Liechti <bstlng321@gmail.com>
2021-10-04 09:45:17 +0200

    Add test-component as example

--- guide/.vuepress/clientAppEnhance.ts
+++ guide/.vuepress/clientAppEnhance.ts
@@ -1,12 +1,15 @@
 import { defineClientAppEnhance } from '@vuepress/client';
+import ButtonTest from './components/ButtonTest.vue';
 
-export default defineClientAppEnhance(({ router }) => {
+export default defineClientAppEnhance(({ app, router }) => {
   // define redirects. These aren't prerendered and if you're on one and refresh the page, you'll get a 404.
   // ~~to prevent this you have to define the redirects on your webserver as well.~~
   // ~~in GitHub Pages, you'd do this with https://spa-github-pages.rafgraph.dev/ probably.~~
   // Amazingly, it already works out of the Box (probably VuePress being awesome again).
   // The only thing not accounted for is SEO, as described here: https://github.com/rafgraph/spa-github-pages#seo
   
+  app.component('ButtonTest', ButtonTest);
+
   router.addRoute({
     path: '/rules/characters/',
     redirect: '/rules/characters/villager.html',
